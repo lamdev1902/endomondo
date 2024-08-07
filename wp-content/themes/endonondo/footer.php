87cafcfc8515056fc624403b404fa7577835b0cc
truclam1902 <nguyenhongtruclam1@gmail.com>
2024-08-07 20:28:50 +0700

    footer endomondo

--- wp-content/themes/endonondo/footer.php
+++ wp-content/themes/endonondo/footer.php
@@ -1,45 +1,116 @@
-<footer id="footer">	
+<footer id="footer">
 	<div class="ft-top">
-		<div class="container">
-			<div class="ft-info list-flex flex-middle flex-center">
-				<div class="ft-logo"><a href="<?php echo home_url(); ?>"><img src="<?php echo get_field('logo','option') ?>" alt=""></a></div>
-				<div class="ft-form">
-					<div class="klaviyo-form-TcfuNL"></div>
+		<div class="container list-flex flex-two">
+			<div class="ft-info item-flex">
+				<div class="mr-bottom-20 ft-logo"><a href="<?php echo home_url(); ?>"><img
+							src="<?php echo get_field('logo', 'option') ?>" alt=""></a></div>
+				<div class="social mr-bottom-40">
+					<?php
+					$social = get_field('social', 'option');
+					if ($social) {
+						foreach ($social as $social) {
+							?>
+							<a target="_blank" href="<?php echo $social['link']; ?>"><img
+									src="<?php echo $social['icon']; ?>" /></a>
+						<?php }
+					} ?>
 				</div>
-				<div class="social">
-					<?php 
-						$social = get_field('social', 'option');
-						if($social){
-							foreach($social as $social){
-					?>
-					<a target="_blank" href="<?php echo $social['link']; ?>"><img src="<?php echo $social['icon'];?>" /></a>
-					<?php }} ?>
+				<div class="ft-form">
+					<div class="title mr-bottom-20">
+						<p class="has-large-font-size">
+							Join over 225,000 Subscribers to Our Weekly Newsletter
+						</p>
+					</div>
+					<div class="description mr-bottom-20">
+						<p class="has-small-font-size">
+							Endomondo newsletter has tips, stories & resources that are all about your mental health and
+							well-being.
+						</p>
+					</div>
+					<div class="klaviyo-form-TcfuNL mr-bottom-20"></div>
 				</div>
 			</div>
-			<nav class="ft-menu">
-				<?php 
-					wp_nav_menu(array(
+			<nav class="ft-menu item-flex">
+				<?php
+				wp_nav_menu(
+					array(
 						'theme_location' => 'menu_cat',
-					));
+					)
+				);
 				?>
 			</nav>
 		</div>
 	</div>
-	<div class="ft-bottom text-center">
-		<div class="container">
-			<?php echo get_field('footer_bottom','option') ?>
+	<div class="ft-bottom">
+		<div class="container list-flex flex-two">
+			<div class="item-flex">
+				<?php echo get_field('footer_bottom', 'option') ?>
+			</div>
+			<div class="item-flex">
+				<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu_footer',
+						)
+					);
+				?>
+			</div>
 		</div>
 	</div>
-</footer>	
+</footer>
 </div>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery-3.5.0.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/slick/slick.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/swiper/swiper-bundle.min.js"></script>
+<script type="text/javascript"
+	src="<?php echo get_template_directory_uri(); ?>/assets/js/swiper/swiper-bundle.min.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/custom.js?v=1.0.5"></script>
 <script async type="text/javascript" src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=RG9krj"></script>
 <?php
-	$pty = get_post_type();
+$pty = get_post_type();
+$args = array(
+	'post_type' => 'gp_elements',
+	'posts_per_page' => 5,
+	'meta_query' => array(
+		array(
+			'key' => 'emposition',
+			'value' => 'before_close_body'
+		),
+		array(
+			'key' => 'emdisplay',
+			'value' => sprintf('"%s"', $pty),
+			'compare' => 'LIKE'
+		)
+	)
+);
+$the_query = new WP_Query($args);
+while ($the_query->have_posts()):
+	$the_query->the_post();
+	echo get_field('emcode', $post->ID, false, false);
+endwhile;
+wp_reset_query();
+$args = array(
+	'post_type' => 'gp_elements',
+	'posts_per_page' => 5,
+	'meta_query' => array(
+		array(
+			'key' => 'emposition',
+			'value' => 'before_close_body'
+		),
+		array(
+			'key' => 'display_with_id_$_pid',
+			'value' => get_the_ID(),
+			'compare' => '='
+		)
+	)
+);
+$the_query = new WP_Query($args);
+while ($the_query->have_posts()):
+	$the_query->the_post();
+	echo get_field('emcode', $post->ID, false, false);
+endwhile;
+wp_reset_query();
+if (is_front_page()) {
 	$args = array(
 		'post_type' => 'gp_elements',
 		'posts_per_page' => 5,
@@ -50,81 +121,43 @@
 			),
 			array(
 				'key' => 'emdisplay',
-				'value' => sprintf('"%s"', $pty),
+				'value' => 'home_page',
 				'compare' => 'LIKE'
 			)
 		)
 	);
- 	$the_query = new WP_Query( $args );
-	while ($the_query->have_posts() ) : $the_query->the_post();
-	echo get_field('emcode',$post->ID, false,false);
+	$the_query = new WP_Query($args);
+	while ($the_query->have_posts()):
+		$the_query->the_post();
+		echo get_field('emcode', $post->ID, false, false);
 	endwhile;
 	wp_reset_query();
-	$args = array(
-		'post_type' => 'gp_elements',
-		'posts_per_page' => 5,
-		'meta_query' => array(
-			array(
-				'key' => 'emposition',
-				'value' => 'before_close_body'
-			),
-			array(
-				'key' => 'display_with_id_$_pid',
-				'value' => get_the_ID(),
-				'compare' => '='
-			)
+}
+$args = array(
+	'post_type' => 'gp_elements',
+	'posts_per_page' => 5,
+	'meta_query' => array(
+		array(
+			'key' => 'emposition',
+			'value' => 'ads_footer'
+		),
+		array(
+			'key' => 'emdisplay',
+			'value' => sprintf('"%s"', $pty),
+			'compare' => 'LIKE'
 		)
-	);
- 	$the_query = new WP_Query( $args );
-	while ($the_query->have_posts() ) : $the_query->the_post();
-	echo get_field('emcode',$post->ID,false, false);
-	endwhile;
-	wp_reset_query();
-	if(is_front_page()) {
-		$args = array(
-			'post_type' => 'gp_elements',
-			'posts_per_page' => 5,
-			'meta_query' => array(
-				array(
-					'key' => 'emposition',
-					'value' => 'before_close_body'
-				),
-				array(
-					'key' => 'emdisplay',
-					'value' => 'home_page',
-					'compare' => 'LIKE'
-				)
-			)
-		);
-	 	$the_query = new WP_Query( $args );
-		while ($the_query->have_posts() ) : $the_query->the_post();
-		echo get_field('emcode',$post->ID,false, false);
-		endwhile;
-		wp_reset_query();
-	}
-	$args = array(
-		'post_type' => 'gp_elements',
-		'posts_per_page' => 5,
-		'meta_query' => array(
-			array(
-				'key' => 'emposition',
-				'value' => 'ads_footer'
-			),
-			array(
-				'key' => 'emdisplay',
-				'value' => sprintf('"%s"', $pty),
-				'compare' => 'LIKE'
-			)
-		)
-	);
- 	$the_query = new WP_Query( $args );
-	while ($the_query->have_posts() ) : $the_query->the_post();
+	)
+);
+$the_query = new WP_Query($args);
+while ($the_query->have_posts()):
+	$the_query->the_post();
+	?>
+	<div class="ads-script"><?php echo get_field('emcode', $post->ID, false, false); ?></div>
+<?php
+endwhile;
+wp_reset_query();
 ?>
-<div class="ads-script"><?php echo get_field('emcode',$post->ID,false, false); ?></div>
-<?php 
-	endwhile;
-	wp_reset_query(); 
-?>
-<?php wp_footer();?>
+<?php wp_footer(); ?>
 </body>
+
 </html>
