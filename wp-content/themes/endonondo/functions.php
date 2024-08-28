27942176a816bd56979e7f2cb8f29f52279d9ce4
truclam1902 <nguyenhongtruclam1@gmail.com>
2024-08-28 14:32:02 +0700

    update version responsive css

--- wp-content/themes/endonondo/functions.php
+++ wp-content/themes/endonondo/functions.php
@@ -168,7 +168,7 @@ function theme_mcs_scripts()
 	wp_enqueue_style('style-custom', get_template_directory_uri() . '/assets/css/custom.css', '', '1.3.0');
 	wp_enqueue_style('style-base', get_template_directory_uri() . '/assets/css/base.css', '', '1.3.0');
 	wp_enqueue_style('style-element', get_template_directory_uri() . '/assets/css/element.css', '', '1.3.9');
-	wp_enqueue_style('style-responsive', get_template_directory_uri() . '/assets/css/responsive.css', '', '1.4.1');
+	wp_enqueue_style('style-responsive', get_template_directory_uri() . '/assets/css/responsive.css', '', '1.4.2');
 	wp_enqueue_style( 'style-awesome', get_template_directory_uri() . '/assets/fonts/css/fontawesome.css');
 	wp_enqueue_style( 'style-solid', get_template_directory_uri() . '/assets/fonts/css/solid.css');
 	wp_enqueue_style( 'style-regular', get_template_directory_uri() . '/assets/fonts/css/regular.css');
