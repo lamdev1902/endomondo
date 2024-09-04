dca78f06ac209df259ecbe9f4f9ffe97aea7d775
truclam1902 <nguyenhongtruclam1@gmail.com>
2024-09-04 15:34:17 +0700

    fix style tool

--- wp-content/themes/endonondo/functions.php
+++ wp-content/themes/endonondo/functions.php
@@ -181,7 +181,7 @@ function theme_mcs_scripts()
 	wp_enqueue_style('style-main', get_template_directory_uri() . '/assets/css/main.css', '', '1.6.3');
 	wp_enqueue_style('style-custom', get_template_directory_uri() . '/assets/css/custom.css', '', '1.3.3');
 	wp_enqueue_style('style-base', get_template_directory_uri() . '/assets/css/base.css', '', '1.3.4');
-	wp_enqueue_style('tool-css', get_template_directory_uri() . '/shortcode/calorie/assets/css/tool.css', '', '1.0.1');
+	wp_enqueue_style('tool-css', get_template_directory_uri() . '/shortcode/calorie/assets/css/tool.css', '', '1.0.2');
 	wp_enqueue_style('style-element', get_template_directory_uri() . '/assets/css/element.css', '', '1.5.1');
 	wp_enqueue_style('style-responsive', get_template_directory_uri() . '/assets/css/responsive.css', '', '1.6.1');
 	wp_enqueue_style( 'style-awesome', get_template_directory_uri() . '/assets/fonts/css/fontawesome.css');
